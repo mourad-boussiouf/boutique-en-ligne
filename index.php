e3a81f98c2264e734a87ff5691c10966e73532a6
mourad-boussiouf <mourad.boussiouf@laplateforme.io>
2022-04-06 17:04:33 +0200

    ajout index provisoire et premiere page du panel admin

--- index.php
+++ index.php
@@ -6,9 +6,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Welcome to the gouffre</title>
 </head>
-<body>
-<form method = POST,>
 
+<body>
+<header><form action="post"><input type="text" id="name" name="name" required
+       minlength="4" maxlength="8" placeholder ="se connecter" size="10"> </form></header>
+
+
+
+
+    <h1> Bienvenue <h1>
 
 
 
