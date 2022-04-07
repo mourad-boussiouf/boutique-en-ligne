ddfd7dee054150d2c67f1ecc2a3979605c6dbd21
mourad-boussiouf <mourad.boussiouf@laplateforme.io>
2022-04-07 06:01:43 +0200

    autoloader et htacces OK, énumérations des éléments de la BDD en train d'etre mis en place en vu du panel admin.

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
-<?php
 
-// pk il marche l'autoloader le frerrre
+<?php
 
 session_start();
 spl_autoload_register(function ($class) {
@@ -20,38 +19,28 @@ spl_autoload_register(function ($class) {
 
 );
 
-// constate du path pour utiliser path partout
 
-define('path', '/boutique-en-ligne/');
+//path = racine du dossier
+define('path', '/versionquimarchepd/');
+//root = remplace l'url par le nom du fichier
 define('ROOT', str_replace('index.php', '', $_SERVER['SCRIPT_FILENAME']));
+//root = créer l'url en cassant l'array du GET
 $params = explode('/', $_GET['p']);
+
+//on verifie les parametres
+if ($params[0] == 'produits') {
+    if (isset($params[1])) {
+        $souC = new SouscategorieModel();
+        $souCate = $souC->getOne('name', $params[1]);
+        $cat = new CategorieModel();
+        $cate = $cat->getOne('name_categories', $params[1]);
+        if (count($souCate) > 0) {
+            Produits::selectBySc($params[1]);
+        } elseif (count($cate) > 0) {
+            Produits::selectByCat($params[1]);
+        }else {
+            Produits::index();
+        }
+    }
+}
 ?>
-
-
-
-
-<a href="admin">Admin la mine</a> 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Welcome to the gouffre</title>
-</head>
-
-<body>
-<header>
- 
-
-
-
-
-<form action="post"><input type="text" id="name" name="name" required
-       minlength="4" maxlength="8" placeholder ="se connecter" size="10"> </form></header>
-
-    <h1> Bienvenue <h1>
-
-</form>
-</body>
-</html>
