f9704f41dd3996b42872dce70e44fb4ed7084d86
mourad-boussiouf <mourad.boussiouf@laplateforme.io>
2022-04-11 02:55:13 +0200

    reparametrage de l'autoloader

--- index.php
+++ index.php
@@ -18,15 +18,15 @@
 
 session_start();
 spl_autoload_register(function ($class) {
-    if (file_exists('handling/' . $class . '.php')) {
-        require_once('handling/' . $class . '.php');
+    if (file_exists('controller/' . $class . '.php')) {
+        require_once('controller/' . $class . '.php');
 
     }
-    if (file_exists('handling/' . $class . 'Class.php')) {
-        require_once('handling/' . $class . 'Class.php');
+    if (file_exists('model/' . $class . 'Model.php')) {
+        require_once('model/' . $class . 'Model.php');
     }
-    if (file_exists('handling/' . $class . '.php')) {
-        require_once('handling/' . $class . '.php');
+    if (file_exists('model/' . $class . '.php')) {
+        require_once('model/' . $class . '.php');
     }
 
 }
