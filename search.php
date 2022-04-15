377b995204bf84e924a0444764766979db911e98
omar-diane <omar.diane@laplateforme.io>
2022-04-15 10:13:58 +0200

    Barre de recherche pour produit + mise en place page contact

---
+++ search.php
@@ -0,0 +1,16 @@
+<?php
+Class Search
+{
+
+    function __construct(){}
+
+    public function searchAll($search)
+    {
+        $sql = "SELECT * FROM products WHERE INSTR( product_name , :search )
+                OR INSTR( product_description, :search);
+                SELECT * FROM products WHERE INSTR( category_id, :search );
+                SELECT * FROM products WHERE INSTR( subcategory_id , :search);";
+
+        $params = [':search' => $search];
+    }
+}
