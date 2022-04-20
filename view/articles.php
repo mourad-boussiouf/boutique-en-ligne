55d482583af9dc5373e6a4665d109900328fe6ee
mourad-boussiouf <mourad.boussiouf@laplateforme.io>
2022-04-20 13:18:28 +0200

    conditionnement du view d'article individuel en préparation de sa back end

--- view/articles.php
+++ view/articles.php
@@ -1,13 +1,19 @@
 
+<?php $pageask = explode('/', $_GET['p']); 
+var_dump($pageask[0]);
+var_dump($pageask[1]);
+var_dump($pageask[2]);?>
 
+
+        <!-- Contient la liste d'articles etcatégories + navigation et tri dans les articles et catégories -->
+<?php if (empty($pageask[1])): ?>
+<div class = browseArticles>
 <?php foreach ($categorie as $value): ?> <!-- Recherche les catégories en bdd -->
                     <option value="<?= $value['name']; ?>"><?= $value['name']; ?></option> <!-- Affiche les catégories -->
 <?php endforeach; ?>
 
 
 
-<?php if (!isset($produit)): ?>
-        <!-- Contient la liste d'articles + pagination -->
         <div class="boxallproducts">
             <h1 class="titre">Découvrez notre collection</h1>
             <div class="boxlisteproduit">
@@ -28,4 +34,11 @@
                 <?php endforeach; ?>
             </div>
         </div>
+
+</div>
+<?php endif; ?>
+
+        <!-- Page d'article individuelle -->
+<?php if (is_numeric($pageask[1])): ?>
+<p>caca</p>
 <?php endif; ?>
