1c13339af4c6113274e1c08220a6d22cefb7d9f1
omar-diane <omar.diane@laplateforme.io>
2022-04-21 16:02:40 +0200

    Début

---
+++ art_individuel.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nom du produit</title>
+    <link rel="stylesheet" href="boutique.css">
+</head>
+<body>
+    <?php
+    require('header.php');
+    ?>
+
+    <main>
+
+<div class="gallery">
+  <a target="_blank" href="#">
+    <img src="pictures/T-shirt-noir.jpg" alt="T-shirt noir" width="600" height="400">
+  </a>
+  <div class="desc"> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officiis fugiat quasi quos reiciendis laborum reprehenderit facilis sint dignissimos, rem corporis perspiciatis? 
+      Cupiditate rerum nostrum corrupti nisi quo, similique rem autem.</div>
+</div>
+
+    </main>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,13 @@
+<header class="main-head">
+        <nav>
+            <ul>
+                <li><a href="index.php">Accueil</a></li>
+                <li><a href="#">Nouveautés</a></li>
+                <li><a href="#">T-shirt</a></li>
+                <li><a href="#">A propos</a></li>
+                <li><a href="authentification.php">Authentification</a></li>
+                <li><a href="#"><i class="fa-solid fa-arrow-right-to-bracket"></i></a></li>
+            </ul>
+            <input type="search" placeholder="Rechercher">
+        </nav>
+    </header>
