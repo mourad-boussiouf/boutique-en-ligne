7e173422c544a591a846b3872e21412aebe15637
mourad-boussiouf <mourad.boussiouf@laplateforme.io>
2022-04-22 12:33:43 +0200

    ajout carousel html css page acceuil avec produits phares

--- view/accueil.php
+++ view/accueil.php
@@ -32,12 +32,40 @@
 
 <h1>Bienvenue sur les gouffrés.max</h1>
 
-<p>Nos produits phares : </p>
 
 
+          <div class = leGouffre> 
+            <span>L</span>
+            <span>e</span>
+            <span>G</span>
+            <span>o</span>
+            <span>u</span>
+            <span>f</span>
+            <span>f</span>
+            <span>r</span>
+            <span>e</span>
+          </div>
+  
+    <h2>Nos produits phares</h2>
+  
 
+    <div class="slider-container">
+      <div class="menu">
+        <label for="slide-dot-1"></label>
+        <label for="slide-dot-2"></label>
+        <label for="slide-dot-3"></label>
+      </div>
+      
+      <input class="slide-input" id="slide-dot-1" type="radio" name="slides" checked>
+      <img class="slide-img" src="<?= path ?>assets/images/phare1.png">
 
+      <input class="slide-input" id="slide-dot-2" type="radio" name="slides">
+      <img class="slide-img" src="<?= path ?>assets/images/tshirt2.jpg">
+      
+      <input class="slide-input" id="slide-dot-3" type="radio" name="slides">
+      <img class="slide-img" src="<?= path ?>assets/images/tshirt3.jpg">
 
+    </div>
 
 
 </html>
