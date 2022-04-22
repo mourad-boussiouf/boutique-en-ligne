0263911a56ec3eb41b3046f42660cfc6534bbfe7
omar-diane <omar.diane@laplateforme.io>
2022-04-22 15:06:16 +0200

    Mise en place du footer

---
+++ footer.php
@@ -0,0 +1,10 @@
+<link rel="stylesheet" href="boutique.css">
+<footer class="footer">
+<table>
+    <tr>
+        <td>SHOP</td>
+        <td>COMPANY</td>
+        <td>NEWSLETTER</td>
+    </tr>
+</table>
+</footer>
