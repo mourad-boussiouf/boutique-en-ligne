8e3d50d14273485a904c00c6e79432488987076a
mourad-boussiouf <mourad.boussiouf@laplateforme.io>
2022-04-24 00:35:21 +0200

    gestion flexbox front CSS page articles done responsive

--- view/articles.php
+++ view/articles.php
@@ -1,19 +1,37 @@
 
 <?php $pageask = explode('/', $_GET['p']); 
 ?>
-    <?php foreach ($categorie as $value): ?> <!-- Recherche les catégories en bdd -->
-                    <option value="<?= $value['name']; ?>"><?= $value['name']; ?></option> <!-- Affiche les catégories -->
-    <?php endforeach; ?>
 
-
-        <!-- Contient la liste d'articles etcatégories + navigation et tri dans les articles et catégories -->
 <?php if (empty($pageask[1])): ?>
 <div class = productscontainer>
 
 
     <div class = container>
-
+        <div class = filters>
             <h1 class="titre">Tous les produits</h1>
+        <!-- Contient la liste d'articles etcatégories + navigation et tri dans les articles et catégories -->
+        
+        
+        <form action="" method="POST">
+        <div class = selectsfilter>
+                <span>Type :</span>
+                <span><select name="id_cat">
+                <?php foreach ($categorie as $key=>$value ): ?>
+                <option value="<?=$value['id']?>"><?=$value['name']?></option>
+                <?php endforeach;?>
+                </select></span>
+                <span>Collection : </span>
+                <span><select name="id_scat">
+                <?php foreach ($scategorie as $key=>$value ): ?>
+                <option value="<?=$value['id']?>"><?=$value['name']?></option>
+                <?php endforeach;?>
+                </select></span>
+        
+                <span id = filterbutton><input type="submit" name="scat" value="Filtrer"></span>
+        </div>
+        </form>        
+        </div>
+
             <div class="boxlisteproduit">
                 <?php foreach ($prod as $value): ?> <!-- Recherche les produits en bdd -->
                     
@@ -23,7 +41,7 @@
                             </a>
                             <h3><?= $value['price']; ?> €</h3>
                             <p class="descriptionproduct"><?= $value['descr'] ?></p>
-                            <form action="#" method="post" name="ajouter">
+                            <form action="<?= path ?>articles/<?= $value['id'] ?>" method="post" name="ajouter">
                             <input type="hidden" name="hidden" value="<?= $value['id'] ?>">
                             <input type="submit" name="achat" value="Acheter">
                         </form>
