ef3a72432adc7bdb56e8a522c686b371a7a9d860
mourad-boussiouf <mourad.boussiouf@laplateforme.io>
2022-05-01 00:43:08 +0200

    front end page paiement terminée

--- view/paiement.php
+++ view/paiement.php
@@ -32,22 +32,28 @@
    
 </div>
 
- <div id = adressconfirm> <span> La commande sera livrée à cette adresse :</span>
-<span><?=$_SESSION['nom']?> <?=$_SESSION['prenom']?></span>
-<span><?=$_SESSION['adresse']?></span>
-<span><a href="<?=path?>mesinfos">Changer mon adresse</a></span></div>
+     <div id = adressconfirm><span id = "actadresstitle"> La commande sera livrée à cette adresse :</span>
+    <span><?=$_SESSION['nom']?> <?=$_SESSION['prenom']?></span>
+    <span><?=$_SESSION['adresse']?></span>
+    <a href="<?=path?>mesinfos">Changer mon adresse</a>
+    <div id = paymentconfirm>
+    <form action = "" method = "POST">
+    <label for="moyen_paiement">Numero de carte bleue :</label>
+    <input type="text" id="metlasans" name = "metlasans" placeholder="5356 1274 4200 1337">
+    <label for="moyen_paiement">Moyen de paiement :</label>
+    <select name="moyen_paiement">
+    <option value="Paypal">Paypal</option>
+    <option value="Mastercard">Mastercard</option>
+    <option value="VISA">VISA</option>
+    <option value="Bitcoin">Bitcoin</option>
+    </select>
+    <input type="submit" id = "confirmpay" name = "confirmpay" value = "Confirmer le paiement">
+    </form>
+    </div>
+    </div>
 
 
 
-<form action = "" method = "POST">
-<label for="moyen_paiement">Moyen de paiement :</label>
-<select name="moyen_paiement">
-<option value="Paypal">Paypal</option>
-<option value="CB">CB</option>
-<option value="Bitcoin">Bitcoin</option>
-</select>
-<input type="submit" id = "confirmpay" name = "confirmpay" value = "Confirmer le paiement">
-</form>
 </div>
 
 
