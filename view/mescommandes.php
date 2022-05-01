5c3eb62b1c07e0aa26c3222fdf8e50c4f41089ed
mourad-boussiouf <mourad.boussiouf@laplateforme.io>
2022-05-01 05:53:35 +0200

    utilisation d'un for pour supprimé temporairement les ID inutiles

--- view/mescommandes.php
+++ view/mescommandes.php
@@ -1,1 +1,35 @@
 <?php var_dump($ordersOfUser) ?>
+
+
+<?php
+for ($i=0; $i < count($ordersOfUser)  ; $i++) {       
+    array_shift($ordersOfUser[$i]); 
+    array_shift($ordersOfUser[$i]);           
+        }
+?>
+
+<?php var_dump($ordersOfUser) ?>
+
+<table class = tablecartname>
+  <tr>
+  <th>Dernieres commandes :</th>
+  </tr>
+
+    <?php foreach ($ordersOfUser[0] as $value): ?>
+        <tr>
+        <td>
+        <?=$value?>
+        </td>
+        </tr>
+    <?php endforeach; ?>
+
+    <?php foreach ($ordersOfUser[1] as $value): ?>
+        <tr>
+        <td>
+        <?=$value?>
+        </td>
+        </tr>
+    <?php endforeach; ?>
+
+
+</table> 
