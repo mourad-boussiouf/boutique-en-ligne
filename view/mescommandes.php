842b73a62838ac94af72b33283f07703fa691ca6
mourad-boussiouf <mourad.boussiouf@laplateforme.io>
2022-05-01 06:09:44 +0200

    construction de la page mescommndes

--- view/mescommandes.php
+++ view/mescommandes.php
@@ -10,26 +10,30 @@ for ($i=0; $i < count($ordersOfUser)  ; $i++) {
 
 <?php var_dump($ordersOfUser) ?>
 
+  <h2>Vos dernières commandes :</h2>
+  
 <table class = tablecartname>
-  <tr>
-  <th>Dernieres commandes :</th>
-  </tr>
 
-    <?php foreach ($ordersOfUser[0] as $value): ?>
-        <tr>
-        <td>
-        <?=$value?>
-        </td>
-        </tr>
-    <?php endforeach; ?>
 
-    <?php foreach ($ordersOfUser[1] as $value): ?>
-        <tr>
-        <td>
-        <?=$value?>
+  <tr> Votre commande du <?=$ordersOfUser[0]['date']?>  </tr>
+    
+        
+        <tr> Contenu de la commande : </tr>
+        <td><?=$ordersOfUser[0]['orderline']?></td>
+
+        <tr> Montant totale de la commande :</tr>
+        <td><?=$ordersOfUser[0]['totalprice']?></td>
+
+        <tr> Coordonnées de la commande :</tr>
+        </td><?=$ordersOfUser[0]['delivery_adress']?></td>
+
+        <td> Statut de la commande (mis à jour) :
+        <?=$ordersOfUser[0]['order_status']?>
         </td>
-        </tr>
-    <?php endforeach; ?>
+        
+    
+
+
 
 
 </table> 
